65d0462f474a922d786dc1f3a549a1462bd91662
Adam Laycock <adam@arcath.net>
2018-01-22 12:50:53 +0000

    do stats per shard

--- arcos/lib/stats.ts
+++ arcos/lib/stats.ts
@@ -3,66 +3,73 @@ import {Utils} from './utils'
 
 export const Stats = {
   build(kernel: Kernel){
-    if(!Memory.stats){ Memory.stats = {}}
+    //if(!Memory.stats){ Memory.stats = {}}
 
-    Memory.stats['gcl.progress'] = Game.gcl.progress
-    Memory.stats['gcl.progressTotal'] = Game.gcl.progressTotal
-    Memory.stats['gcl.level'] = Game.gcl.level
-    Memory.stats['cpu.getUsed'] = Game.cpu.getUsed()
-    Memory.stats['cpu.limit'] = Game.cpu.limit
-    Memory.stats['cpu.bucket'] = Game.cpu.bucket
-    Memory.stats['cpu.kernelLimit'] = kernel.limit
-    Memory.stats['memory.size'] = RawMemory.get().length
-    Memory.stats['market.credits'] = Game.market.credits
+    let stats: {[key: string]: number} = {}
 
-    Memory.stats['processes.counts.total'] = Object.keys(kernel.processTable).length
-    Memory.stats['processes.counts.run'] = kernel.execOrder.length
-    Memory.stats['processes.counts.suspend'] = kernel.suspendCount
-    Memory.stats['processes.counts.missed'] = (Object.keys(kernel.processTable).length - kernel.execOrder.length - kernel.suspendCount)
-    Memory.stats['processes.counts.faulted'] = 0
+    stats['gcl.progress'] = Game.gcl.progress
+    stats['gcl.progressTotal'] = Game.gcl.progressTotal
+    stats['gcl.level'] = Game.gcl.level
+    stats['cpu.getUsed'] = Game.cpu.getUsed()
+    stats['cpu.limit'] = Game.cpu.limit
+    stats['cpu.bucket'] = Game.cpu.bucket
+    stats['cpu.kernelLimit'] = kernel.limit
+    stats['memory.size'] = RawMemory.get().length
+    stats['market.credits'] = Game.market.credits
 
-    if(Memory.stats['processes.counts.missed'] < 0){
-      Memory.stats['processes.counts.missed'] = 0
+    stats['processes.counts.total'] = Object.keys(kernel.processTable).length
+    stats['processes.counts.run'] = kernel.execOrder.length
+    stats['processes.counts.suspend'] = kernel.suspendCount
+    stats['processes.counts.missed'] = (Object.keys(kernel.processTable).length - kernel.execOrder.length - kernel.suspendCount)
+    stats['processes.counts.faulted'] = 0
+
+    if(stats['processes.counts.missed'] < 0){
+      stats['processes.counts.missed'] = 0
     }
 
     _.forEach(Object.keys(kernel.processTypes), function(type){
-      Memory.stats['processes.types.' + type] = 0
+      stats['processes.types.' + type] = 0
     })
 
-    Memory.stats['processes.types.undefined'] = 0
-    Memory.stats['processes.types.init'] = 0
-    Memory.stats['processes.types.flagWatcher'] = 0
+    stats['processes.types.undefined'] = 0
+    stats['processes.types.init'] = 0
+    stats['processes.types.flagWatcher'] = 0
 
     _.forEach(kernel.execOrder, function(execed: {type: string, cpu: number, faulted: boolean}){
-      Memory.stats['processes.types.' + execed.type] += execed.cpu
+      stats['processes.types.' + execed.type] += execed.cpu
 
       if(execed.faulted){
-        Memory.stats['processes.counts.faulted'] += 1
+        stats['processes.counts.faulted'] += 1
       }
     })
 
-    Memory.stats['processes.types.scheduler'] = kernel.schedulerUsage
-    Memory.stats['processes.types.kernel-scheduler'] = 0
+    stats['processes.types.scheduler'] = kernel.schedulerUsage
+    stats['processes.types.kernel-scheduler'] = 0
 
     _.forEach(Object.keys(kernel.data.roomData), function(roomName){
       let room = Game.rooms[roomName]
 
       if(room.controller && room.controller.my){
-        Memory.stats['rooms.' + roomName + '.rcl.level'] = room.controller.level
-        Memory.stats['rooms.' + roomName + '.rcl.progress'] = room.controller.progress
-        Memory.stats['rooms.' + roomName + '.rcl.progressTotal'] = room.controller.progressTotal
-        Memory.stats['rooms.' + roomName + '.rcl.percentage'] = (room.controller.progress / room.controller.progressTotal) * 100
-        Memory.stats['rooms.' + roomName + '.ramparts.target'] = Utils.rampartHealth(kernel, roomName)
+        stats['rooms.' + roomName + '.rcl.level'] = room.controller.level
+        stats['rooms.' + roomName + '.rcl.progress'] = room.controller.progress
+        stats['rooms.' + roomName + '.rcl.progressTotal'] = room.controller.progressTotal
+        stats['rooms.' + roomName + '.rcl.percentage'] = (room.controller.progress / room.controller.progressTotal) * 100
+        stats['rooms.' + roomName + '.ramparts.target'] = Utils.rampartHealth(kernel, roomName)
 
-        Memory.stats['rooms.' + roomName + '.spawn.energy'] = room.energyAvailable
-        Memory.stats['rooms.' + roomName + '.spawn.energyTotal'] = room.energyCapacityAvailable
+        stats['rooms.' + roomName + '.spawn.energy'] = room.energyAvailable
+        stats['rooms.' + roomName + '.spawn.energyTotal'] = room.energyCapacityAvailable
 
         if(room.storage){
-          Memory.stats['rooms.' + roomName + '.storage.energy'] = room.storage.store.energy
+          stats['rooms.' + roomName + '.storage.energy'] = room.storage.store.energy
         }else{
-          Memory.stats['rooms.' + roomName + '.storage.energy'] = 0
+          stats['rooms.' + roomName + '.storage.energy'] = 0
         }
       }
     })
+
+    let json: {[shard: string]: any} = {}
+    json[Game.shard.name] = stats
+
+    RawMemory.segments[90] = JSON.stringify(json)
   }
 }
